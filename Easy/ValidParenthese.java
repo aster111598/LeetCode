edfdf18458a8f3a500ddfe78e9cb217570f035fe
aster111598 <illikegumi@gmail.com>
2025-07-27 22:39:45 +0800

    20題VaildParenthere完成

--- Easy/ValidParenthese.java
+++ Easy/ValidParenthese.java
@@ -1,8 +1,10 @@
 package Easy;
 
+import java.util.Stack;
+
 public class ValidParenthese {
     public static void main(String[] args) {
-        String testBrackets = "()";
+        String testBrackets = "()]";
 
         if (Validator(testBrackets)) {
             System.out.println("true");
@@ -12,11 +14,34 @@ public class ValidParenthese {
     }
 
     public static boolean Validator(String testBrackets) {
-        // Check the bracket length is even number or not
-        if (testBrackets.length() % 2 != 0) {
-            return false;
+        // 條件:
+        // 1. 左括號推入stack，遇到右括號則pop top檢查是不是一對
+        // 2. stack要為空才行
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < testBrackets.length(); i++) {
+            char ch = testBrackets.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+            // 遇到右括號，且有可能stack已經空了要return false
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || (ch == ']' && top != '[') || (ch == '}' && top != '{')) {
+                    return false;
+                }
+            }
         }
 
-        return true;
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
